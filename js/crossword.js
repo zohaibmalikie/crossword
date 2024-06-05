1700e139d47657b38b7a54056bed092b5f59cfc3
Zohaib Malik <zohaibmalikie@gmail.com>
2024-06-05 05:27:17 +0500

    temp click

--- js/crossword.js
+++ js/crossword.js
@@ -979,9 +979,16 @@ function triggerInput(selectedCell){
 
     if(selectedCell && supportsTouch()){
         var key_interceptor = document.getElementById('key_interceptor');
-        key_interceptor.focus();
+        key_interceptor.style.opacity = '1'; // Make the input visible
+        
         key_interceptor.style.left = selectedCell.x + 'px';
         key_interceptor.style.top = selectedCell.y  + 'px';
+        key_interceptor.focus();
+
+        key_interceptor.addEventListener('click', function tempClick() {
+            key_interceptor.focus();
+            key_interceptor.removeEventListener('click', tempClick);
+        });
     }
 
 }
